c5ca2e0dd819fe8461262bc8aec3ec06750c06a4
Oswaldo <oswalramigu@gmail.com>
2024-05-01 12:35:11 -0500

    codificación módulo con Hibernate y Spring

---
+++ agenda_cita/src/main/java/com/oswalsena/agenda/agenda_cita/AgendaCitaApplication.java
@@ -0,0 +1,13 @@
+package com.oswalsena.agenda.agenda_cita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaCitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaCitaApplication.class, args);
+	}
+
+}
---
+++ agenda_cita/src/main/java/com/oswalsena/agenda/agenda_cita/Repositories/ServicioRepository.java
@@ -0,0 +1,8 @@
+package com.oswalsena.agenda.agenda_cita.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.oswalsena.agenda.agenda_cita.Entities.Servicio;
+
+public interface ServicioRepository extends JpaRepository<Servicio, Long> {
+
+}
